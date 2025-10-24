a3f521bafb2093dab263c4e89d289c27994b3b3c
Alidabi10 <112751131+Alidabi10@users.noreply.github.com>
2025-10-24 08:30:47 +0200

    Delete main function from Wektor2D.hpp
    Removed main function and related vector operations.

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -29,39 +29,3 @@ public:
 
 	double operator*(const Wektor2D& other) { return x * other.x + y * other.y; }
 };
-
-
-
-
-int main()
-{
-	Wektor2D v1{};	
-	v1.setX(1.);
-	v1.setY(1.);
-	double x1 = v1.getX();
-	double y1 = v1.getY();
-//	std::println("Wartosc wspolrzednej x1: {}. Wartosc wspolrzednej y1: {}.", x1, y1);
-	Wektor2D v2{ 2., 2. };
-	std::println("Wartosc wspolrzednej x2: {}. Wartosc wspolrzednej y2: {}.", v2.getX(), v2.getY());
-	Wektor2D sum = v1 + v2;
-//	std::println("Suma wektorow: x_suma = {}, y_suma = {}.", sum.getX(), sum.getY());
-	double prod = v1 * v2;
-//	std::println("Iloczyn skalarny wektorow wynosi: {}.", prod);
-
-
-	/*
-	Wektor2D v1{};           // Konstruktor domyślny, wektor o wsp. [0, 0]
-	v1.setX(1.);             // setter
-	v1.setY(1.);             // setter
-	double x1 = v1.getX();   // getter
-	double y1 = v1.getY();   // getter
-
-	Wektor2D v2{ 2., 2. }; // Konstruktor nadający współrzędne
-
-	Wektor2D sum = v1 + v2; // dodawanie wektorów
-
-	double prod = v1 * v2; // iloczyn skalarny
-	*/
-	return 0;
-}
-
